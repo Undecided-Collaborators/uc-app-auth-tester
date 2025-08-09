3cb8c0025a7af5f354215067a0edbd0c7d513f07
Iyola Oyabiyi O <iyolaoyabiyi@gmail.com>
2025-08-09 03:44:36 +0100

    fix: fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'uc-app-auth-tester'
+  base: '/uc-app-auth-tester/'
 })
